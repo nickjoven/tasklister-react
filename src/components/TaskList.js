8bece7153809f2ff7e0cbc354e368a1d90f1de87
Nicholas <nicholasjoven@gmail.com>
2022-07-24 13:36:09 -0400

    complete rep 3

---
+++ src/components/TaskList.js
@@ -0,0 +1,17 @@
+import Task from './Task'
+
+const TaskList = ({tasks}) => {
+    return (
+        <div className='Task-List'>
+            <h3>My To-Do's</h3>
+            <ul>
+                {tasks.map((element, index) => {
+                    return (
+                        <Task key={index} text={element} />
+                    )
+                })}
+            </ul>
+        </div>
+            )
+}
+export default TaskList

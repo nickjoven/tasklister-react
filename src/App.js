540f60e04ccb50fd91a3feb12c1e15f8511048d7
Nicholas <nicholasjoven@gmail.com>
2022-07-24 17:52:49 -0400

    finished rep 09

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import Banner from './components/Banner'
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
 
 const App = () => {
+  const [tasks, setTasks] = useState([])
 
+  const handleSubmit = (text) => {
+    setTasks([...tasks, text])
+  }
 
   return (
     <div className="App">
-
+      <Banner bannerText='Task Lister React' />
+      <TaskForm handleSubmit={handleSubmit} />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
